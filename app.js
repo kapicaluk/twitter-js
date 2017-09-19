67f592dacdfff07ba6d0cdf363319928145053a3
kapicaluk <kapicaluk@gmail.com>
2017-09-19 11:07:20 -0400

    starting a server, logging middleware, logging responses

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+
+const server = http.createServer();
+
+const PORT = 3000; 
+
+server.listen(PORT,function(){
+	console.log('server listening. . . ')
+});
+
+server.on('request', app);
+
+app.use(function(req, res, next){
+	console.log('logged', req.method, res.statusCode);
+	next();
+});
+
+app.get('/', function(req,res){
+	res.send('Hello world')
+})
+
+app.get('/news', function(req, res){
+	res.send('Fake news')
+})
+
+
+
+
